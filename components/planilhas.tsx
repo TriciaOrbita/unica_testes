77163348f4e69de75b4b608831c9c3bf0ef638b4
Trícia Tapajós <mpasselewe@gmail.com>
2024-10-28 17:57:52 -0300

    Test: Testando a conexão da planilha do CadUnico com a interface

---
+++ components/planilhas.tsx
@@ -0,0 +1,31 @@
+// components/Planilhas.tsx
+import React from 'react';
+import Papa from 'papaparse';
+import { FamilyData } from '../interfaces/familyData'; 
+
+interface PlanilhasProps {
+  onImportData: (data: FamilyData[]) => void; // Use o tipo definido
+}
+
+const Planilhas: React.FC<PlanilhasProps> = ({ onImportData }) => {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      Papa.parse<FamilyData>(file, {
+        header: true,
+        complete: (results) => {
+          // Chama a função para atualizar os dados no componente pai
+          onImportData(results.data);
+        },
+      });
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+    </div>
+  );
+};
+
+export default Planilhas;
